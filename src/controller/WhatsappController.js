826094a82b1d04e512b75365bb668253476ce2d5
Celso Bitello <celsokaiiki@gmail.com>
2026-05-19 00:11:30 -0300

    W02 - Criando controle principal do App

---
+++ src/controller/WhatsappController.js
@@ -0,0 +1,12 @@
+class WhatsappController {
+    constructor() {
+        this.loadElements();
+    }
+
+    loadElements() {
+        this.el = {};
+        document.querySelectorAll('[id]').forEach(element=>{
+            this.el[Format.getCamelCase(element.id)] = element;
+        });
+    }
+}

83b9f62e7a4fcf331d3daf9e8a67228dd0e95dc2
Celso Bitello <celsokaiiki@gmail.com>
2026-05-21 01:36:31 -0300

    W14 - Criando um servidor Web com Webpack Dev Server

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,11 +1,11 @@
 class CameraController {
     constructor(videoEl) {
         this._videoEl = videoEl;
-
+//utilizado para ativar a câmera
         navigator.mediaDevices.getUserMedia({
             video: true
         }).then(stream=>{
-            this._videoEl.srcObject = stream;
+            this._videoEl.srcObject = stream; 
             this._videoEl.play();
         }).catch(err=>{
             console.error(err);

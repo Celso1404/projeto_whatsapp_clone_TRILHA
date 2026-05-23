66c1ad43148f93161187fcd1618c8668340f069e
Celso Bitello <celsokaiiki@gmail.com>
2026-05-23 00:31:31 -0300

    W24 - Criando evento para visualizar o tempo de gravação

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -49,6 +49,7 @@ export class MicrophoneController extends ClassEvent{
                 }); 
             });
             this._mediaRecorder.start();
+            this.startTimer();
         }
     }
 
@@ -56,6 +57,18 @@ export class MicrophoneController extends ClassEvent{
         if(this.isAvailable()) {
             this._mediaRecorder.stop();
             this.stop();
+            this.stopTimer();
         }
+    }
+
+    startTimer() {
+        let start = Date.now(); 
+        this._recordMicrophoneInterval = setInterval(() => {
+            this.trigger('recordtimer', (Date.now() - start));
+        }, 100);
+    }
+
+    stopTimer() {
+        clearInterval(this._recordMicrophoneInterval);
     }
 }
--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -240,11 +240,13 @@ export class WhatsappController {
         this.el.btnSendMicrophone.on('click', e=> {
             this.el.recordMicrophone.show();
             this.el.btnSendMicrophone.hide();
-            this.startRecordMicrophoneTime();
             this._microphoneController = new MicrophoneController();
             this._microphoneController.on('ready', audio=>{
                 this._microphoneController.startRecorder();
             });
+            this._microphoneController.on('recordtimer', timer=>{
+                this.el.recordMicrophoneTimer.innerHTML = Format.toTime(timer); 
+            })
         });
         this.el.btnCancelMicrophone.on('click', e=>{
             this._microphoneController.stopRecorder();
@@ -308,21 +310,12 @@ export class WhatsappController {
 
                 this.el.inputText.dispatchEvent(new Event('keyup'));
             });
-        });
-    }
-
-    //Começa a gravar o microfone
-    startRecordMicrophoneTime() {
-        let start = Date.now(); 
-        this._recordMicrophoneInterval = setInterval(() => {
-            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start); 
-        }, 100);
+        });   
     }
     //Fechar microfone
     closeRecordMicrophone() {
         this.el.recordMicrophone.hide();
         this.el.btnSendMicrophone.show();
-        clearInterval(this._recordMicrophoneInterval)
     }
 
     closeAllMainPanel() { //funciona para esconder os painéis

1d00d38901ba5f4fc6e9dd370c62464b41f68514
Celso Bitello <celsokaiiki@gmail.com>
2026-05-28 02:02:31 -0300

    W45 - Gravando e enviando Audio como anexo

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -535,6 +535,15 @@ export class WhatsappController {
             this.closeRecordMicrophone();
         });
         this.el.btnFinishMicrophone.on('click', e=>{
+            this._microphoneController.on('recorded', (file, metadata)=> {
+                Message.sendAudio(
+                    this._contactActive.chatId,
+                    this._user.email,
+                    file,
+                    metadata,
+                    this._user.photo
+                ) 
+            });
             this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
         });
